0e7da136ff6d9d705f100cf7caf16250a3e23373
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-10-18 03:03:13 +0400

    banner section added

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,26 @@
+import Aos from "aos";
+import { useEffect } from "react";
+import 'aos/dist/aos.css'
+const Banner = () => {
+
+    useEffect(()=>{
+        Aos.init()
+    }, [])
+    return (
+        <div>
+            <div data-aos="fade-down-right" className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/SmrsLW1/banner-car.jpg)' }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div  data-aos="zoom-out-up" className="max-w-md">
+                        <p className="uppercase mb-5 text-sky-600 text-4xl">Top brands</p>
+                        <h1 className="mb-5 text-5xl uppercase font-bold">Modern-Classic</h1>
+                        <p className="mb-5 uppercase text-xl">Get 30% off on selected items</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Components/Layouts/Root.jsx
+++ src/Components/Layouts/Root.jsx
@@ -5,7 +5,7 @@ import Footer from "./Footer";
 
 const Root = () => {
     return (
-        <div className="max-w-7xl m-3 mx-auto">
+        <div className="max-w-7xl m-3 mx-auto min-h-screen">
             <Navbar></Navbar>
             <div className="max-w-6xl m-3 mx-auto">
             <Outlet></Outlet>
