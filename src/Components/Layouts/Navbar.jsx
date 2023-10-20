c5fa2871424fae2c1f3975f8eea4fd7f505481f5
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-10-21 02:28:34 +0400

    dark light theme added

--- src/Components/Layouts/Navbar.jsx
+++ src/Components/Layouts/Navbar.jsx
@@ -1,44 +1,59 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
-import {FaUserDoctor} from "react-icons/fa6";
+import { FaUserDoctor } from "react-icons/fa6";
 
 
 const Navbar = () => {
-const {user, logOut} = useContext(AuthContext);
+    const [theme, setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light");
+    const { user, logOut } = useContext(AuthContext);
+    
+    const handleToggleTheme = e =>{
+         if(e.target.checked){
+            setTheme("dark");
+         }else{
+            setTheme("light")
+         }
+    }
+    useEffect(()=>{
+        localStorage.setItem("theme", theme)
+        const localTheme = localStorage.getItem("theme");
+        document.querySelector("html").setAttribute("data-theme", localTheme)
+    },[theme])
 
 
-const handleSignOut = () =>{
-    logOut()
-    .then(result=>{
-        console.log(result.user)
-      
-    })
-    .catch(error=>{
-        console.error(error)
-    })
-}
+
+    const handleSignOut = () => {
+        logOut()
+            .then(result => {
+                console.log(result.user)
+
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }
 
     const navLinks = <>
-     <li> <NavLink to='/' 
-     style={isActive => ({
-    color: isActive ? "blue" : "black"
-  })}>Home</NavLink></li>
-     
-     <li><NavLink to='/addProducts' 
-     style={isActive => ({
-    color: isActive ? "blue" : "black"
-  })}>Add Product</NavLink></li>
-     
-     <li> <NavLink to='/myCart' 
-     style={isActive => ({
-    color: isActive ? "blue" : "black"
-  })}>My Cart</NavLink></li>
-     
-     <li><NavLink to='/login' 
-     style={isActive => ({
-    color: isActive ? "blue" : "black"
-  })}>Login</NavLink></li>
+        <li> <NavLink to='/'
+            style={isActive => ({
+                color: isActive ? "blue" : "black"
+            })}>Home</NavLink></li>
+
+        <li><NavLink to='/addProducts'
+            style={isActive => ({
+                color: isActive ? "blue" : "black"
+            })}>Add Product</NavLink></li>
+
+        <li> <NavLink to='/myCart'
+            style={isActive => ({
+                color: isActive ? "blue" : "black"
+            })}>My Cart</NavLink></li>
+
+        <li><NavLink to='/login'
+            style={isActive => ({
+                color: isActive ? "blue" : "black"
+            })}>Login</NavLink></li>
     </>
     return (
         <div>
@@ -53,9 +68,9 @@ const handleSignOut = () =>{
                         </ul>
                     </div>
                     <div className="flex justify-center items-center gap-0">
-                    {/* <img className="w-32 border" src="https://i.ibb.co/5YqP24z/logo.webp" alt="" /> */}
-                    <img className="w-11 md:w-28 rounded-full" src="https://i.ibb.co/dkZm9H8/preview.png" alt="" />
-                    <div className="btn btn-ghost text-base md:text-2xl font-bold border">Dream <span className="text-sky-600">Auto</span></div>
+                        {/* <img className="w-32 border" src="https://i.ibb.co/5YqP24z/logo.webp" alt="" /> */}
+                        <img className="w-11 md:w-28 rounded-full" src="https://i.ibb.co/dkZm9H8/preview.png" alt="" />
+                        <div className="btn btn-ghost text-base md:text-2xl font-bold border">Dream <span className="text-sky-600">Auto</span></div>
                     </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
@@ -63,34 +78,49 @@ const handleSignOut = () =>{
                         {navLinks}
                     </ul>
                 </div>
-                
-                
+
+                {/* toggle theme*/}
+
+                <label className="swap swap-rotate">
+
+                    {/* this hidden checkbox controls the state */}
+                    <input type="checkbox" onChange={handleToggleTheme} checked={theme === "light" ? false : true}/>
+
+                    {/* sun icon */}
+                    <svg className="swap-on fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
+
+                    {/* moon icon */}
+                    <svg className="swap-off fill-current w-10 h-10" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" /></svg>
+
+                </label>
+
+
                 <div className="navbar-end">
 
-                  
-                {
-                    user ?
-                      <button onClick={handleSignOut} className="btn">Sign Out</button>
-                    :
-                    <Link to='/login'>
-                    <button className="btn">Login</button>
-                    </Link>
-                }
-                 {
-                    user ? <span>{user.email}
-                       <p>{user.displayName}</p>
-                        <img className="w-10 rounded" src={user.photoURL} alt=""/>
-                         <FaUserDoctor className="rounded w-10"></FaUserDoctor>
-                         </span> : ''
-                }
-                
-                
-                    
-                    
-                    
-                    
+
+                    {
+                        user ?
+                            <button onClick={handleSignOut} className="btn">Sign Out</button>
+                            :
+                            <Link to='/login'>
+                                <button className="btn">Login</button>
+                            </Link>
+                    }
+                    {
+                        user ? <span>{user.email}
+                            <p>{user.displayName}</p>
+                            <img className="w-10 rounded" src={user.photoURL} alt="" />
+                            <FaUserDoctor className="rounded w-10"></FaUserDoctor>
+                        </span> : ''
+                    }
+
+
+
+
+
+
                 </div>
-                
+
             </div>
         </div>
     );
